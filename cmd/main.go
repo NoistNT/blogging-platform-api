e958a366a46009983d8a263caa4a06470dfb74a9
NoistNT <arielgnr23@gmail.com>
2024-10-16 12:48:59 -0300

    Add migration to main and update routers

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 
+	"github.com/NoistNT/blogging-platform-api/cmd/migrate"
 	"github.com/NoistNT/blogging-platform-api/config"
 	"github.com/NoistNT/blogging-platform-api/internal/handlers"
 	"github.com/gin-gonic/gin"
@@ -18,7 +19,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Connect to the databse
+	// Connect to the database
 	conn, err := pgx.Connect(context.Background(), cfg.DBURL)
 	if err != nil {
 		log.Fatalf("unable to connect to database: %v\n", err)
@@ -30,9 +31,23 @@ func main() {
 		log.Fatalf("unable to ping database: %v\n", err)
 	}
 
-	router.POST("/posts", handlers.CreatePost)
-	router.GET("/posts", handlers.GetPosts)
-	router.GET("/posts/:id", handlers.GetPost)
+	// Run migration
+	if err := migrate.Migrate(conn); err != nil {
+		log.Fatalf("failed to migrate database: %v\n", err)
+	}
+
+	router.POST("/posts", func(c *gin.Context) {
+		handlers.CreatePost(c, conn)
+	})
+	router.GET("/posts", func(c *gin.Context) {
+		handlers.GetPosts(c, conn)
+	})
+	router.GET("/posts/:id", func(c *gin.Context) {
+		handlers.GetPost(c, conn)
+	})
+	router.DELETE("/posts/:id", func(c *gin.Context) {
+		handlers.RemovePost(c, conn)
+	})
 
 	log.Fatalf("failed to start server: %v", router.Run(":8080"))
 }
